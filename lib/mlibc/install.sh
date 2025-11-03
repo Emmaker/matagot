9147a45850b89dca9c5789a19b38d54e35ed1ff7
emmaker <emmaker@myyahoo.com>
2025-11-03 15:14:37 -0500

    Finish script

--- lib/mlibc/install.sh
+++ lib/mlibc/install.sh
@@ -1,11 +1,13 @@
 TMP=$(mktemp)
 (cd builddir && meson introspect --install-plan) >$TMP
 
-jq -c '.targets | keys[]' $TMP | while read -r KEY; do
-    OUT=$(jq -r ".targets[].$KEY.destination" $TMP)
-    OUT=$(echo "$OUT" | sed -e "s|{libdir}|$BUILD/lib|g" -e "s|{libdir_shared}|$DEPLOY/lib|g")
+jq -r '.targets | keys[]' $TMP | while read -r KEY; do
+    OUT=$(jq -r ".targets[\"$KEY\"].destination" $TMP)
+    OUT=$(echo $OUT | sed -e "s|{libdir}|$BUILD/lib|g" -e "s|{libdir_shared}|$DEPLOY/lib|g")
 
-    echo $OUT
+    echo "Installing $OUT..."
+    mkdir -p $(dirname $OUT)
+    cp -u $KEY $OUT
 done
 
 rm -f $TMP
